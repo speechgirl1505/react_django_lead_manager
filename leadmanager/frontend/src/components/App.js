bcae08f7e51cc52d8db661c543f7caea9f22df8c
Kala Elam <speechgirl1505@gmail.com>
2020-04-22 16:16:18 -0500

    added elements to frontend, fixed capitalization error and missind load static in index

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,20 @@
-import React, {Component} from 'react';
-import ReactDom from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDom from "react-dom";
+
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
 
 class App extends Component {
-    render(){
-        return <h1>React App</h1>
-    }
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById("app"));
