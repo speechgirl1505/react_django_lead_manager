d4ef42fb68717c86a815b13c922eb8bb9609476e
Kala Elam <speechgirl1505@gmail.com>
2020-04-25 23:48:24 -0500

    back end auth, login-logout-register

--- leadmanager/accounts/serializers.py
+++ leadmanager/accounts/serializers.py
@@ -4,26 +4,31 @@ from django.contrib.auth import authenticate
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields =('id', 'username', 'email')
-
+  class Meta:
+    model = User
+    fields = ('id', 'username', 'email')
 
 # Register Serializer
-
 class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = { 'password': {'write_only': True}}
+  class Meta:
+    model = User
+    fields = ('id', 'username', 'email', 'password')
+    extra_kwargs = {'password': {'write_only': True}}
 
-def create(self, validate_data):
-    user = User.objects.create_user(
-    validate_data['username'], 
-    validate_data['email'], 
-    validate_data['password'])
+  def create(self, validated_data):
+    user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
 
-    return User
+    return user
 
 # Login Serializer
+class LoginSerializer(serializers.Serializer):
+  username = serializers.CharField()
+  password = serializers.CharField()
+
+  def validate(self, data):
+    user = authenticate(**data)
+    if user and user.is_active:
+      return user
+    raise serializers.ValidationError("Incorrect Credentials")
+
 
